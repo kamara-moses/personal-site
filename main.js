c64765e5281987a4e0d3b3c41f6472219691ec77
Moses Kamara <kamara.moses@yahoo.com>
2020-08-25 17:28:45 -0700

    final commit for now

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // target icon
-const targetHam = document.querySelector(".ham-icon");
+const targetHam = document.querySelector('.ham-icon');
 
 // target menu
-const targetMenu = document.querySelector(".active");
+const targetMenu = document.querySelector('.active');
 
 // event
 targetHam.onclick = (e) => {
     e.preventDefault();
-    targetMenu.classList.toggle("active");
+    targetMenu.classList.toggle('active');
 };
